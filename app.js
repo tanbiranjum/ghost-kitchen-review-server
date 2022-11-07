5b94ba24887b84ae5f44ce82e9f19198fa290f18
tanbiranjum <tanbiranjum@hotmail.com>
2022-11-07 22:23:02 +0600

    api route added

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const cors = require('cors');
-const express = require('express');
+const cors = require("cors");
+const express = require("express");
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
+
+const kitchenRoutes = require("./routes/kitchenRoutes");
+const reviewRoutes = require("./routes/reviewRoutes");
+
+app.use("/api/v1/kitchens", kitchenRoutes);
+app.use("/api/v1/reviews", reviewRoutes);
 
 module.exports = app;
