345e2d6bfdebe6b8534248532a19600412b9e835
tanbiranjum <tanbiranjum@hotmail.com>
2022-11-09 13:06:48 +0600

    error handling functionality added

--- app.js
+++ app.js
@@ -15,4 +15,24 @@ app.use("/api/v1/kitchens", kitchenRoutes);
 app.use("/api/v1/reviews", reviewRoutes);
 app.use("/api/v1/auth", authRoutes);
 
+// 404 Error Handler
+app.use((req, res, next) => {
+  const error = new Error(`Not found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    next("There was an error");
+  } else {
+    if (err.message) {
+      res.status(500).send(err.message);
+    } else {
+      res.send("There was an error");
+    }
+  }
+});
+
 module.exports = app;
--- index.js
+++ index.js
@@ -15,12 +15,24 @@ mongoose.connection.once("error", () => {
 const PORT = process.env.PORT || 5000;
 
 function startServer() {
-  mongoose.connect(process.env.MONGO_URL);
+  mongoose.connect(process.env.MONGO_URL).catch((error) => {
+    console.log(error);
+  });
   const server = http.createServer(app);
 
   server.listen(PORT, () => {
     console.log(`App is listening on port ${PORT}`);
   });
+
+  return server;
 }
 
-startServer();
+const server = startServer();
+
+process.on("uncaughtException", (error) => {
+  console.log(error.name, error.message);
+  console.log("UNCAUGHT EXCEPTION! 💥💥💥 Shutting down...");
+  server.close(() => {
+    process.exit(1);
+  });
+})
